725b6280dc398f27425e3d1547ac564e40c57184
Joonas Rautiola <joonas.rautiola@unikie.com>
2024-09-17 11:36:42 +0300

    Test change
    Signed-off-by: Joonas Rautiola <joonas.rautiola@unikie.com>

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 # Copyright 2022-2024 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
 {
-  description = "Ghaf Framework: Documentation and implementation for TII SSRC Secure Technologies";
+  description = "Ghaf Framework: Documentation and implementation for TII SSRC Secure Technologiess";
 
   nixConfig = {
     substituters = [
